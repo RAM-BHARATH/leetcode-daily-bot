222de35f8791d14372c66d8699e6de4b957ad47e
RAM-BHARATH <ramtherapid007@gmail.com>
2024-01-09 23:12:47 +0530

    Added graphql request to get question

--- scraper.py
+++ scraper.py
@@ -19,20 +19,54 @@ class Question:
 
 
 def fetch_question():
-  print('fetch question')
-  res = requests.get(URL)
-  soup = BeautifulSoup(res.text, 'html.parser')
-  print('running scrapper...')
-  # print(res.text)
-  # print(soup.find(id='__NEXT_DATA__'))
-  next_data = soup.find(id='__NEXT_DATA__')
-  next_data = next_data.get_text()
-  next_data = json.loads(next_data)
-  # print(next_data)
-  todays_challenge = next_data['props']['pageProps']['dehydratedState'][
-      'queries'][-1]['state']['data']['dailyCodingChallengeV2']['challenges'][
-          -1]
-  # print(todays_challenge)
+  # print('fetch question')
+  # res = requests.get(URL)
+  # soup = BeautifulSoup(res.text, 'html.parser')
+  # print('running scrapper...')
+  # # print(res.text)
+  # # print(soup.find(id='__NEXT_DATA__'))
+  # next_data = soup.find(id='__NEXT_DATA__')
+  # next_data = next_data.get_text()
+  # next_data = json.loads(next_data)
+  # # print(next_data)
+  # todays_challenge = next_data['props']['pageProps']['dehydratedState'][
+  #     'queries'][-1]['state']['data']['dailyCodingChallengeV2']['challenges'][
+  #         -1]
+  # # print(todays_challenge)
+
+  query = '''{dailyCodingChallengeV2(year: 2024, month: 1) {  challenges {
+        date
+        userStatus
+        link      
+        question {        
+            questionFrontendId        title        titleSlug      
+            }    
+        }
+    }}'''
+
+    # '''{dailyCodingChallengeV2(year: 2024, month: 1) {  challenges {
+    #     date
+    #     userStatus
+    #     link      
+    #     question {        
+    #         questionFrontendId        title        titleSlug      
+    #         }    
+    #     }    
+    #     weeklyChallenges {      
+    #         date      
+    #         userStatus     
+    #         link     
+    #         question {        
+    #             questionFrontendId       
+    #             title        
+    #             titleSlug      
+    #             isPaidOnly     
+    #         }   
+    #     }  
+    # }}'''
+  res = requests.post(URL+'graphql', json={'query': query})
+  todays_challenge = res['data']['dailyCodingChallengeV2']['challenge'][-1]
+
 
   question = Question(todays_challenge)
   print(question)
